b781727955ed8043e2649f4676a5a61c0e41c212
danial <danial@mmpz.ir>
2021-06-30 03:07:42 +0430

    vet models

---
+++ pet-clinic-data/src/main/java/com/hospital/petclinic/service/VetService.java
@@ -0,0 +1,6 @@
+package com.hospital.petclinic.service;
+
+import com.hospital.petclinic.model.Vet;
+
+public interface VetService extends BaseService<Vet> {
+}
---
+++ pet-clinic-data/src/main/java/com/hospital/petclinic/service/VetServiceImpl.java
@@ -0,0 +1,22 @@
+package com.hospital.petclinic.service;
+
+import com.hospital.petclinic.model.Vet;
+
+import java.util.Set;
+
+public class VetServiceImpl implements VetService {
+    @Override
+    public Vet findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return null;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return null;
+    }
+}
