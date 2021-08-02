44f687b383f2ea57bbc4a3f3adf1f926e72b3347
danial <danial@mmpz.ir>
2021-08-02 20:17:17 +0430

    jpa first phase

--- pet-clinic-data/src/main/java/com/hospital/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/hospital/petclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.hospital.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -14,4 +21,5 @@ public class BaseEntity implements Serializable {
         this.id = id;
         return this;
     }
+
 }
