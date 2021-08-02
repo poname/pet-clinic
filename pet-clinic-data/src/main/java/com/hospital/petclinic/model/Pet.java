37d64f41e35a5533f38981a5f7e57ea18b6cf641
danial <danial@mmpz.ir>
2021-08-02 20:29:17 +0430

    jpa second phase

--- pet-clinic-data/src/main/java/com/hospital/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/hospital/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.hospital.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -54,6 +59,15 @@ public class Pet extends BaseEntity {
 
     public Pet setOwner(Owner owner) {
         this.owner = owner;
+        return this;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public Pet setVisits(Set<Visit> visits) {
+        this.visits = visits;
         return this;
     }
 }
